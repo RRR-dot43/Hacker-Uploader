9dc32164f270671b47b0ba2b850cfb47292d3d04
RRR-dot43 <ruhilrohit10@gmail.com>
2025-05-19 16:14:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 api_id =  25967358
 api_hash  = '10a5a31171dfdc323efdbcf84a8fb016'
 bot_token =  '8151035412:AAGnfaXf0-gUKtVdaPU-kUZC-VcPaJC0AA8'
-LOG =  -100253268913
+LOG =  -1002452084505
